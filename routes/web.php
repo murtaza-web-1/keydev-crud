8914cd65b3e3776050fc2176a1716965f2710a93
murtaza-web-1 <mrkhan77707@gmail.com>
2025-04-26 16:15:10 -0700

    final-touch

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 // USER dashboard - without auth (or you can add auth if you want)
